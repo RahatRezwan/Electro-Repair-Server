d894ba4fb86388b1179444f0918f79e28145e382
Kazi Rahat <kazirahat1007@outlook.com>
2022-11-08 17:18:32 +0600

    mongo db connected with server

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -11,6 +12,20 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
    res.send("electro repair server is running");
+});
+
+/* connect mongodb */
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.9tctcbt.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+   serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+   const collection = client.db("test").collection("devices");
+   // perform actions on the collection object
+   client.close();
 });
 
 app.listen(port, () => {
