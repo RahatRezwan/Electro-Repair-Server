ed9702093b43eb36ef0d48a2c54b23605464af89
Kazi Rahat <kazirahat1007@outlook.com>
2022-11-08 17:10:36 +0600

    add middleware and configuration dotenv

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+/* Middleware */
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
    res.send("electro repair server is running");
